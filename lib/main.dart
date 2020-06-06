ef2e148232fc5288146a9aad0c4aa221e8bc1b0a
mayakaconard <mayakaconard@gmail.com>
2020-06-06 03:25:36 +0300

    Added Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,48 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home()
+  home: Home(),
 
 ));
 
 class Home extends StatelessWidget {
   @override
+
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Conard App'),
         centerTitle: true,
         backgroundColor: Colors.indigo[900],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.indigo[900],
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
+        ),
       ),
       body: Center(
        child: Icon(
